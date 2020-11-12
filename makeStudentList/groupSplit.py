a6c15d6d746ffd48999e4e2b9bda3385ecad2589
hrdkdh <hrdkdh@naver.com>
2020-11-12 21:38:22 +0900

    Update groupSplit.py

--- makeStudentList/groupSplit.py
+++ makeStudentList/groupSplit.py
@@ -37,37 +37,46 @@ def splitGroup(cha_name):
     print("차수명 : " + cha_name)
     df = checkClipboard()
     print("자동 조편성 후 엑셀파일로 작성하는 중...")
-    print(df)
-    #총 조 갯수 계산
-    total_group_cnt = round(len(df)/group_member_number_limit)
-
-    #우선순위대로 정렬
-    order_cit_arr = ["대학명", "계열", "성별", "거주지_시", "거주지_도"]
-    df.sort_values(by=order_cit_arr, inplace=True)
-    df.reset_index(drop=True, inplace=True) #인덱스 리셋
-
-    for i in range(group_member_number_limit):
-        this_start_num = i*total_group_cnt
-        this_end_num = this_start_num+total_group_cnt
-        group_number = 0
-        for idx in range(this_start_num, this_end_num):
-            group_number += 1
-            df._set_value(idx, "조", group_number)
+    # print(df)
     
-    order_cit_arr2 = ["조", "성명"]
-    df.sort_values(by=order_cit_arr2, inplace=True)
-    df.reset_index(drop=True, inplace=True) #인덱스 리셋
+    #반은 몇개인가?
+    ban_arr = df["반"].unique()
 
-    print_no = 0
-    for idx in df.index:
-        print_no = print_no+1
-        if print_no > group_member_number_limit:
-            print_no = 1
-        df._set_value(idx, "출력순서", print_no)
+    df_arrays = []
+    for ban in ban_arr:
+        this_df = df.query("반 == '" + ban + "'")
+        #총 조 갯수 계산
+        total_group_cnt = round(len(this_df)/group_member_number_limit)
 
+        #우선순위대로 정렬
+        order_cit_arr = ["대학명", "계열", "성별", "거주지_시", "거주지_도"]
+        this_df.sort_values(by=order_cit_arr, inplace=True)
+        this_df.reset_index(drop=True, inplace=True) #인덱스 리셋
+
+        for i in range(group_member_number_limit):
+            this_start_num = i*total_group_cnt
+            this_end_num = this_start_num+total_group_cnt
+            group_number = 0
+            for idx in range(this_start_num, this_end_num):
+                group_number += 1
+                this_df._set_value(idx, "조", group_number)
+        
+        order_cit_arr2 = ["조", "성명"]
+        this_df.sort_values(by=order_cit_arr2, inplace=True)
+        this_df.reset_index(drop=True, inplace=True) #인덱스 리셋
+
+        print_no = 0
+        for idx in this_df.index:
+            print_no = print_no+1
+            if print_no > group_member_number_limit:
+                print_no = 1
+            this_df._set_value(idx, "출력순서", print_no)
+        df_arrays.append(this_df)
+
+    df_results = pd.concat(df_arrays, ignore_index=True)
     makeDownloadDirectory()
 
-    df.to_excel(filesave_path+"조편성표_"+cha_name+".xlsx", sheet_name="조편성 명단")
+    df_results.to_excel(filesave_path+"조편성표_"+cha_name+".xlsx", sheet_name="조편성 명단")
     print("조편성 완료")
     print("다음 폴더에 조편성 파일을 저장하였습니다 : " + filesave_path+"조편성표_"+cha_name+".xlsx")
     
