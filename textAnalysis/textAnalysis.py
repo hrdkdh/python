45eb645dab82a83d760ce5a810ab815231f16923
hrdkdh <36975040+hrdkdh@users.noreply.github.com>
2020-11-20 12:59:21 +0900

    201120 commit

--- textAnalysis/textAnalysis.py
+++ textAnalysis/textAnalysis.py
@@ -5,9 +5,9 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 #PART1 : 데이터 로드
-wb=load_workbook("data.xlsx", data_only=True)
-ws=wb["설문 응답결과"]
-cells=ws["F2:I73"]
+wb=load_workbook("sample_data.xlsx", data_only=True)
+ws=wb["VOC결과"]
+cells=ws["C2:C415"]
 data=""
 for row in cells:
 	for cell in row:
@@ -16,27 +16,21 @@ for row in cells:
 
 kkma=Kkma()
 
-print("단어 형태소 필터링...")
+print("단어 형태소 분석 중... 기다려 주세요")
 dataPos=kkma.pos(data)
 dataArr=[]
 for wordPos in dataPos:
 	word=wordPos[0]
 	pos=wordPos[1]
-	if pos=="NNG" and word!="토":
-		if word=="선배":
-			dataArr.append("멘토/선배")
-		else:
-			dataArr.append(word)
-	elif pos=="NNG" and word=="토":
-		dataArr.append("멘토/선배")
-	elif pos=="VA" and word=="좋":
-		dataArr.append("좋았다")
+	if pos=="NNG" : #명사만 필터링함. 동사도 포함하려면 or pos=="VA" 추가할 것
+		dataArr.append(word)
 
-print("빈도 계산중...")
+print("단어별 발생빈도 계산중...")
 counter=Counter(dataArr).most_common()
 keywords={}
 resultsFile=open("results.txt", "w", encoding="utf-8")
 
+percent = 0
 for keyword in counter:
 	word=keyword[0]
 	freq=keyword[1]
